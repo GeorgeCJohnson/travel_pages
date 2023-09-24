061115a71ec768d773da676e4635c1f26c48095a
George J <Gjohnson.pioneer@gmail.com>
2023-09-24 15:54:29 -0400

    Add session.js to models folder.  Complete sessions file.

---
+++ models/session.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// define custom table for user sessions
+sequelize.define("Session", {
+    sid: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+    },
+    expires: Sequelize.DATE,
+    data: Sequelize.TEXT,
+    user_id: Sequelize.STRING,
+    logged_in: {
+      type: Sequelize.TINYINT(1),
+      defaultValue: false,
+    },
+});
+  
+    function extendDefaultFields(defaults, session) {
+        return {
+        data: defaults.data,
+        expires: defaults.expires,
+        user_id: session.user_id,
+        logged_in: session.logged_in,     
+        };
+    } 
+  
+  module.exports = { extendDefaultFields }; 
