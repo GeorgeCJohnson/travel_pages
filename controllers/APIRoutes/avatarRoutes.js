f98f05a34f32f0d5ea992dc86f0bfeeb03f6b994
benjaminescobar1 <135399618+benjaminescobar1@users.noreply.github.com>
2023-09-25 19:22:05 -0700

    fixing file

---
+++ controllers/APIRoutes/avatarRoutes.js
@@ -0,0 +1,90 @@
+const express = require ("express");
+const router = express.Router();
+const { Avatar } = require("./models"); 
+
+//Create a new avatar
+router.post("/", async (req, res) =>{
+  try{
+      const avatar = await Avatars.create(req.body);
+      //If avatar was successfully created, respond with a 201 created status code
+      res.status(201).json(avatar);
+      //If an error occured during the process, catch and responds with a 400 bad request status code
+  } catch (error) {
+      res.status(400).json({ error: error.message});
+  }
+});
+
+//Delete an avatar by ID
+router.delete('/:id', async (req, res) => {
+try {
+  //Attempts to delete the avatar
+  const deleted = await Avatars.destroy({
+    where: { id: req.params.id },
+  });
+  //Checks if avatar deleted was successful
+  if (deleted) {
+    //Status 204 code if successfully deleted
+    res.status(204).json({ message: 'Avatar deleted' });
+  } else {
+    //Status 404 code if avatar not found
+    res.status(404).json({ error: 'Avatar not found' });
+  }
+  //Catches any errors that occur during process
+} catch (error) {
+  res.status(500).json({ error: error.message });
+}
+});
+
+//Get all avatars
+router.get("/", async (req, res) => {
+    try {
+      const avatars = await Avatars.findAll();
+      //If avatars found successfully, respond with 200 OK status code
+      res.status(200).json(avatars);
+      //If an error occured during the process, catch and responds with a 400 bad request status code
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+//Get specific avatar by id
+router.get("/:id", async (req,res)=>{
+  try {
+    //Attempts to find an avatar in the database by its ID
+    const avatar = await Avatars.findByPk(req.params.id);
+    //If an avatar is found, respond with a 200 OK status and the avatar data
+    if (avatar) {
+      res.status(200).json(avatar);
+      }else{
+        //If no avatar is found, respond with a 404 Not Found status and an error message
+        res.status(404).json({ error: "Avatar not found"});
+      }
+    } catch (error) {
+       //If an error occured during the process, catch and responds with a 400 bad request status code
+      res.status(400).json({ error: error.message});
+    }
+    });
+
+// Update a avatar by id
+router.put("/:id", async (req, res) => {
+  try {
+    // Attempt to update the avatar with the provided data
+    const [updated] = await Avatars.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (updated) {
+      //Fetch the updated avatar from the database if one record was updated
+      const updatedAvatar = await Avatars.findByPk(req.params.id);
+      //Respond with a 200 OK status and the updated avatar data
+      res.status(200).json(updatedAvatar);
+    } else {
+      //If no avatar updated, responds with a 404 Not Found status
+      res.status(404).json({ error: "Avatar not found"});
+    }
+  } catch (error) {
+    //If an error occured during the process, catch and responds with a 400 bad request status code
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
---
+++ controllers/APIRoutes/pinRoutes.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const { Pins, User, Avatars } = require("./models"); 
+
+//GET route to retrieve all pins
+router.get("/", async (req, res) => {
+    try {
+      const pins = await Pins.findAll();
+      res.status(200).json(pins);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+  //GET route to retrieve a specific pin by pin ID
+  router.get("/:pinid", async (req, res) => {
+    try {
+      const pins = await Pins.findByPk(req.params.pinid);
+      if (pins) {
+        res.status(200).json(pins);
+      } else {
+        res.status(404).json({ error: "Pin not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+  //POST route to create a new pin and assign pin to the user
+  router.post("/create/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const newPins = await Pins.create({
+        ...req.body,
+        // user_id: req.session.user_id,
+        user_id: id,
+      });
+      res.status(201).json(newPins);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+  
+  //PUT route to update a pin
+  router.put("/update/:pinid", async (req, res) => {
+    try {
+      const [updated] = await Pins.update(req.body, {
+        where: { id: req.params.pinid },
+      });
+  
+      if (updated !== 0) {
+        const updatedPin = await Pins.findByPk(req.params.pinid);
+        res.status(200).json(updatedPin);
+      } else {
+        const existingPin = await Pins.findByPk(req.params.pinid);
+        if (existingPin) {
+          res.status(200).json({ message: "No update has been made." });
+        } else {
+          res.status(404).json({ error: "Pin not found" });
+        }
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+  
+  //DELETE route to delete a Pin
+  router.delete("/delete/:id", async (req, res) => {
+    try {
+      const pinData = await Pins.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+      if (pinData) {
+        res.status(204).json({ message: "Pin deleted" });
+      } else {
+        res.status(404).json({ error: "Pin not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+  
+  module.exports = router;
