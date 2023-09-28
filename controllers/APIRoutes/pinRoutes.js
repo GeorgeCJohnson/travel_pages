62b4aad4489f12d07d2bf3fe19774f8e5e95c041
George J <Gjohnson.pioneer@gmail.com>
2023-09-28 14:11:10 -0400

    Correcting seeds to add new avatar and pin seed data.

--- controllers/APIRoutes/pinRoutes.js
+++ controllers/APIRoutes/pinRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Pins } = require("../../models");
 const withAuth = require("../../utils/auth");
-=======
+
 
 //GET route to retrieve all pins
 router.get("/", async (req, res) => {
